ccdba31b680aeb15b9aea8e76e7a1084ea3bfa6a
Jonathan Main <Jonathan@uaf53313.ddns.uark.edu>
2017-02-28 20:11:41 -0600

    Updated activity_login.xml. Created activity_create_employee.xml and LoginActivity.java

---
+++ app/src/main/java/edu/uark/uarkregisterapp/LoginActivity.java
@@ -0,0 +1,39 @@
+package edu.uark.uarkregisterapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Query the server here to determine if employees exist in the database?
+
+        super.onCreate(savedInstanceState); // Not sure what this does
+
+
+        setContentView(R.layout.activity_login_landing);    // Loads the XML (UI) file
+    }
+
+    public void loginButtonOnClick(View view) {
+        this.startActivity(new Intent(getApplicationContext(), LandingActivity.class));
+        // This doesn't seem to work right now
+        // Check the database for valid employee login here?
+    }
+
+/*  // Stuff from ActivityLanding.java
+    public void createProductButtonOnClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), ProductViewActivity.class);
+
+        intent.putExtra(
+                getString(R.string.intent_extra_product),
+                new ProductTransition()
+        );
+
+        this.startActivity(intent);
+    }
+*/  // Not sure what this commented chunk does
+}
