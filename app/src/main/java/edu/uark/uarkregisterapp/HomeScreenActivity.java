98ab11a9466f6f2e42407663abb75b228a930360
Jonathan Main <Jonathan@uaf53313.ddns.uark.edu>
2017-03-02 17:45:59 -0600

    Added functionality not available dialogs to home screen

--- app/src/main/java/edu/uark/uarkregisterapp/HomeScreenActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/HomeScreenActivity.java
@@ -1,7 +1,9 @@
 package edu.uark.uarkregisterapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -17,22 +19,22 @@ public class HomeScreenActivity extends AppCompatActivity {
 
 	// Start Transaction Button
 	public void startTransactionButtonOnClick(View view) {
-		// Popup message
+		this.functionalityNotAvailableDialog(view);
 	}
 
 	// Create Employee Button
 	public void createEmployeeButtonOnClick(View view) {
-		// Popup message
+		this.functionalityNotAvailableDialog(view);
 	}
 
 	// Sales Report: Product Button
 	public void viewSalesReportProductButtonOnClick(View view) {
-		// Popup message
+		this.functionalityNotAvailableDialog(view);
 	}
 
 	// Sales Report: Cashier Button
 	public void viewSalesReportCashierButtonOnClick(View view) {
-		// Popup message
+		this.functionalityNotAvailableDialog(view);
 	}
 
 	// Log Out Button
@@ -40,4 +42,20 @@ public class HomeScreenActivity extends AppCompatActivity {
 		this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
 	}
 
+	// Functionality Not Available Dialog
+	public void functionalityNotAvailableDialog(View view) {
+		new AlertDialog.Builder(this).
+				setTitle(R.string.alert_title_functionality_not_available).
+				setMessage(R.string.alert_functionality_not_available).
+				setPositiveButton(
+						R.string.button_dismiss,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.dismiss();
+							}
+						}
+				).
+				create().
+				show();
+	}
 }
