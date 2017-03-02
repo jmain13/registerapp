e82454e92988e7078f615a3a65d001efa85abdd3
Jonathan Main <Jonathan@uaf53313.ddns.uark.edu>
2017-03-02 15:41:54 -0600

    Login button now opens home screen

--- app/src/main/java/edu/uark/uarkregisterapp/LoginActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/LoginActivity.java
@@ -13,27 +13,14 @@ public class LoginActivity extends AppCompatActivity {
         // Query the server here to determine if employees exist in the database?
 
         super.onCreate(savedInstanceState); // Not sure what this does
-
-
         setContentView(R.layout.activity_login);    // Loads the XML (UI) file
     }
 
     public void loginButtonOnClick(View view) {
-        this.startActivity(new Intent(getApplicationContext(), LandingActivity.class));
-        // This doesn't seem to work right now
+
         // Check the database for valid employee login here?
+
+        this.startActivity(new Intent(getApplicationContext(), HomeScreenActivity.class));
     }
 
-/*  // Stuff from ActivityLanding.java
-    public void createProductButtonOnClick(View view) {
-        Intent intent = new Intent(getApplicationContext(), ProductViewActivity.class);
-
-        intent.putExtra(
-                getString(R.string.intent_extra_product),
-                new ProductTransition()
-        );
-
-        this.startActivity(intent);
-    }
-*/  // Not sure what this commented chunk does
 }
